ec230a73ec03de1e52a61eb72afb8a490a10de54
Abraham Meza Vega <abrahammv_6@hotmail.com>
2019-07-25 21:59:31 -0600

    [New] Main window

--- UI/Canvas.py
+++ UI/Canvas.py
@@ -3,42 +3,42 @@ from tkinter import Canvas as tkCanvas
 class Canvas:
 
     def __init__(self, window, width = 854, height = 480):
-        self._instance = tkCanvas(window, width = width, height = height)
-        self._width = width
-        self._height = height
-        self._parent = window
+        self.__instance = tkCanvas(window, width = width, height = height)
+        self.__width = width
+        self.__height = height
+        self.__parent = window
 
     def getWidth(self):
-        return self._width
+        return self.__width
 
     def getHeight(self):
-        return self._height
+        return self.__height
 
     def getParent(self):
-        return self._parent
+        return self.__parent
 
     def pack(self):
-        self._instance.pack()
+        self.__instance.pack()
 
     def deleteElement(self, element):
-        self._instance.delete(element)
+        self.__instance.delete(element)
 
     def drawOval(self, x1, y1, x2, y2, color = '#000000', outline = '#000000'):
-        return self._instance.create_oval(x1, y1, x2, y2, fill = color,
+        return self.__instance.create_oval(x1, y1, x2, y2, fill = color,
                                           outline = outline)
 
     def drawLine(self, x1, y1, x2, y2, arrow = False, color = '#000000',
                  width = 1):
-        return self._instance.create_line(x1, y1, x2, y2, arrow = arrow,
+        return self.__instance.create_line(x1, y1, x2, y2, arrow = arrow,
                                           fill = color, width = width)
     def drawText(self, x, y, text):
-        return self._instance.create_text(x, y, text = text)
+        return self.__instance.create_text(x, y, text = text)
 
     def setClickEvent(self, action):
-        self._instance.bind('<Button-1>', action)
+        self.__instance.bind('<Button-1>', action)
 
     def setMouseMoveEvent(self, action):
-        self._instance.bind('<B1-Motion>', action)
+        self.__instance.bind('<B1-Motion>', action)
 
     def setClickReleaseEvent(self, action):
-        self._instance.bind('<ButtonRelease-1>', action)
+        self.__instance.bind('<ButtonRelease-1>', action)
--- UI/Window.py
+++ UI/Window.py
@@ -6,20 +6,20 @@ from UI.Canvas import Canvas
 class Window():
 
     def __init__(self, name, width = 854, height = 480):
-        self._instance = Tk()
-        self._instance.title(name)
-        self._instance.geometry(str(width) + 'x' + str(height))
+        self.__instance = Tk()
+        self.__instance.title(name)
+        self.__instance.geometry(str(width) + 'x' + str(height))
 
     def setResizable(self, x, y):
-        self._instance.resizable(x, y)
+        self.__instance.resizable(x, y)
 
     def createCanvas(self, width = 854, height = 480):
-        canvas = Canvas(self._instance, width, height)
+        canvas = Canvas(self.__instance, width, height)
         canvas.pack()
         return canvas
 
     def createButton(self, x, y, width, height, text, function):
-        button = Button(self._instance, text = text, command = function)
+        button = Button(self.__instance, text = text, command = function)
         button.place(x = x, y = y, width = width, height = height)
         return button
 
@@ -27,4 +27,4 @@ class Window():
         mainloop()
 
     def close(self):
-        self._instance.destroy()
+        self.__instance.destroy()
